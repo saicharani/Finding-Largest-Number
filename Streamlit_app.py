81d127eae6d0a6ee13c63870ff26ec02c4aab641
saicharani <51985750+saicharani@users.noreply.github.com>
2023-08-31 19:24:00 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,4 @@
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 Largest among 3 numbers 
