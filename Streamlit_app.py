254ba3089137f0c79fcb9522f01523b536565a8f
saicharani <51985750+saicharani@users.noreply.github.com>
2023-08-31 19:36:31 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -6,9 +6,9 @@ Largest among 3 numbers
 #Get Input
 
 def find_largest(num1, num2, num3):
-    if (a>=b) and (a>=c) return a
-    elif (b>=a) and (b>=c) return b
-    else return c
+    if (a>=b) and (a>=c): return a
+    elif (b>=a) and (b>=c): return b
+    else: return c
 
 def main():
     num1 = st.number_input("Enter the first number:")
